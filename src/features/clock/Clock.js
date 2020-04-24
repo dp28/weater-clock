6ea060c2ddbe89b86002bf3f664d55e3667af86f
Daniel Patterson <dan24patt@gmail.com>
2020-04-23 22:15:48 -0400

    Add light ring and loading animation

--- src/features/clock/Clock.js
+++ src/features/clock/Clock.js
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { LightRing } from "../lightRing/LightRing";
 import styles from "./Clock.module.css";
 
-export const Clock = () => (
-  <div className={styles.clock}>
-    <div className={styles.clockContent}>
-      <LightRing lights={buildLights(60)} inset={2} />
-      <LightRing lights={buildLights(12)} />
+export const Clock = ({ loading = true }) => {
+  const [minutes, setMinutes] = useState(buildLights(60));
+  const [hours, setHours] = useState(buildLights(12));
+  const [togglingOn, setTogglingOn] = useState(true);
+
+  useInterval(
+    () => {
+      const lightToToggle = minutes.findIndex(light =>
+        togglingOn ? !light.on : light.on
+      );
+      if (lightToToggle === -1) {
+        setTogglingOn(!togglingOn);
+      }
+      setMinutes(
+        minutes.map((light, index) =>
+          lightToToggle === index ? { ...light, on: togglingOn } : light
+        )
+      );
+    },
+    loading ? 1000 : null
+  );
+
+  return (
+    <div className={styles.clock}>
+      <div className={styles.clockContent}>
+        <LightRing lights={minutes} inset={2} />
+        <LightRing lights={hours} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 function buildLights(number) {
   return [...Array(number)].map((_, index) => ({
     index,
     number: index + 1,
     color: "white",
-    on: index < 20
+    on: false
   }));
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delay !== null) {
+      let id = setInterval(() => savedCallback.current(), delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
