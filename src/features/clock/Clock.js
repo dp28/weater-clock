7c23bb51e02a4326ae6761f3525130392aa5c924
Daniel Patterson <dan24patt@gmail.com>
2020-04-25 12:36:54 -0400

    Add first attempt at colors for weather

--- src/features/clock/Clock.js
+++ src/features/clock/Clock.js
@@ -1,10 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useInterval } from "../../useInterval";
 import { LightRing } from "../lightRing/LightRing";
 import styles from "./Clock.module.css";
 
-export const Clock = ({ loading = true }) => {
-  const [minutes, setMinutes] = useState(buildLights(60));
+const Snow = "white";
+const Clouds = "#9E9E9E";
+const Rain = "#0288D1";
+const Clear = "#FDD835";
+
+const background = {
+  minutes: buildLights(60).map((light, index) => ({
+    ...light,
+    color: [Snow, Clouds, Rain, Clear][Math.floor((4 * index) / 60)],
+    on: true
+  }))
+};
+
+function buildLights(number) {
+  return [...Array(number)].map((_, index) => ({
+    index,
+    number: index + 1,
+    color: "white",
+    on: false
+  }));
+}
+
+export const Clock = ({ loading = false }) => {
+  const [minutes, setMinutes] = useState(
+    loading ? buildLights(60) : background.minutes,
+    [loading]
+  );
   const [hours, setHours] = useState(buildLights(12));
   const [togglingOn, setTogglingOn] = useState(true);
 
@@ -34,12 +59,3 @@ export const Clock = ({ loading = true }) => {
     </div>
   );
 };
-
-function buildLights(number) {
-  return [...Array(number)].map((_, index) => ({
-    index,
-    number: index + 1,
-    color: "white",
-    on: false
-  }));
-}
--- src/features/lightRing/LightRing.js
+++ src/features/lightRing/LightRing.js
@@ -5,6 +5,7 @@ const LightRadius = `min(1vh, 1vw)`;
 
 export const Light = ({ light, degreesPerLight }) => {
   const rotationInDegrees = light.index * degreesPerLight;
+  const color = light.on ? light.color : "white";
 
   return (
     <div
@@ -21,13 +22,13 @@ export const Light = ({ light, degreesPerLight }) => {
     >
       <div
         style={{
-          backgroundColor: light.on ? light.color : null,
+          backgroundColor: light.on ? color : null,
           height: `calc(2 * ${LightRadius})`,
           width: `calc(2 * ${LightRadius})`,
           borderRadius: LightRadius,
           boxShadow: light.on
-            ? `${light.color} 0 0 calc(1.5 * ${LightRadius}) calc(0.2 * ${LightRadius})`
-            : `inset ${light.color} 0 0 calc(1 * ${LightRadius})`
+            ? `${color} 0 0 calc(1.5 * ${LightRadius}) calc(0.2 * ${LightRadius})`
+            : `inset ${color} 0 0 calc(1 * ${LightRadius})`
         }}
       />
     </div>
