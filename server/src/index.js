b13ead457649a7ec8a2c8567b547f9342dc09e16
Daniel Patterson <dan24patt@gmail.com>
2020-04-26 12:42:02 -0400

    Add basic logging

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(morgan("dev"));
 
 app.get("/forecast", (request, response) => {
   response.json({ hello: "world" });
