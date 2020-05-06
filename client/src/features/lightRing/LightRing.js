9e84813b1c285e142455198fc82e75cf142e40b3
Daniel Patterson <dan24patt@gmail.com>
2020-05-06 08:45:54 -0400

    Fix off-by-one error in clock light position

--- client/src/features/lightRing/LightRing.js
+++ client/src/features/lightRing/LightRing.js
@@ -4,20 +4,20 @@ import styles from "./LightRing.module.css";
 const LightRadius = `min(1vh, 1vw)`;
 
 export const Light = ({ light, degreesPerLight }) => {
-  const rotationInDegrees = light.index * degreesPerLight;
+  const rotationInDegrees = (light.index - 1) * degreesPerLight;
   const colour = light.on ? light.colour : "white";
 
   return (
     <div
       className={styles.lightContainer}
       style={{
-        transform: `rotate(${rotationInDegrees -
-          90 +
-          degreesPerLight}deg) translate(calc(50% -  3 * ${LightRadius})) rotate(-${rotationInDegrees +
-          270 +
-          degreesPerLight}deg) translate(calc(-1 * ${LightRadius}), calc(-${light.index *
-          2 +
-          1} * ${LightRadius})`
+        transform: `rotate(${
+          rotationInDegrees - 90 + degreesPerLight
+        }deg) translate(calc(50% -  3 * ${LightRadius})) rotate(-${
+          rotationInDegrees + 270 + degreesPerLight
+        }deg) translate(calc(-1 * ${LightRadius}), calc(-${
+          light.index * 2 + 1
+        } * ${LightRadius})`,
       }}
     >
       <div
@@ -28,7 +28,7 @@ export const Light = ({ light, degreesPerLight }) => {
           borderRadius: LightRadius,
           boxShadow: light.on
             ? `${colour} 0 0 calc(1.5 * ${LightRadius}) calc(0.2 * ${LightRadius})`
-            : `inset ${colour} 0 0 calc(1 * ${LightRadius})`
+            : `inset ${colour} 0 0 calc(1 * ${LightRadius})`,
         }}
       />
     </div>
@@ -42,10 +42,10 @@ export const LightRing = ({ lights, inset = 0 }) => (
       marginLeft: `calc(${inset * 2} * ${LightRadius})`,
       marginTop: `calc(${inset * 2} * ${LightRadius})`,
       width: `calc(100% - ${inset * 4} * ${LightRadius})`,
-      height: `calc(100% - ${inset * 4} * ${LightRadius})`
+      height: `calc(100% - ${inset * 4} * ${LightRadius})`,
     }}
   >
-    {lights.map(light => (
+    {lights.map((light) => (
       <Light
         key={light.index}
         light={light}
