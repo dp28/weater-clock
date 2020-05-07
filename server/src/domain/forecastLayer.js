9c3e71168b5ea808985972336c67f044db21b04c
Daniel Patterson <dan24patt@gmail.com>
2020-05-07 08:40:11 -0400

    Extract forecast layer into separate file

---
+++ server/src/domain/forecastLayer.js
@@ -0,0 +1,86 @@
+const WeatherType = require("weather-type");
+
+const EmptyLayer = {
+  inner: { colours: [] },
+  outer: { colours: [] },
+};
+
+const HourInSeconds = 60 * 60;
+const HalfDayInSeconds = 12 * HourInSeconds;
+const InnerLightsPerHour = 5;
+const SecondsPerLight = HourInSeconds / InnerLightsPerHour;
+
+module.exports.build = (forecast) => {
+  if (!forecast) {
+    return EmptyLayer;
+  }
+
+  return { inner: buildInner(forecast) };
+};
+
+function buildInner(forecast) {
+  const startIndex = calculateStartIndex(forecast.current.dt);
+  const colours = calculateColours(forecast);
+  return { startIndex, colours };
+}
+
+function calculateStartIndex(secondsSinceEpoch) {
+  const secondsToday = secondsSinceEpoch % HalfDayInSeconds;
+  const secondsToStartOfHour = secondsToday - (secondsToday % HourInSeconds);
+  return Math.floor(secondsToStartOfHour / SecondsPerLight);
+}
+
+function calculateColours({ hourly, current }) {
+  return calculateCurrentHourColours(current).concat(
+    calculateHourlyColours(hourly, current.dt)
+  );
+}
+
+function calculateCurrentHourColours(currentForecastPoint) {
+  const colour = calculateColour(currentForecastPoint);
+  return Array(5).fill(colour);
+}
+
+function calculateHourlyColours(hourlyForecasts, currentTime) {
+  const startOfHour = currentTime - (currentTime % HourInSeconds);
+  const firstIndex = hourlyForecasts.findIndex((_) => _.dt > startOfHour);
+  return hourlyForecasts
+    .slice(firstIndex, firstIndex + 11)
+    .map(calculateColour)
+    .flatMap((colour) => Array(5).fill(colour));
+}
+
+function calculateColour(forecastPoint) {
+  const weatherTypes = forecastPoint.weather
+    .map((_) => WeatherType.default.fromString(_.description))
+    .filter((_) => _.isSome)
+    .map((_) => _.unwrap());
+
+  if (willSnow(weatherTypes)) {
+    return "white";
+  } else if (willRain(weatherTypes)) {
+    return "blue";
+  } else if (willBeClear(weatherTypes)) {
+    return "yellow";
+  } else {
+    return "grey";
+  }
+}
+
+function willSnow(weatherTypes) {
+  return weatherTypes.some(
+    (_) =>
+      _.precipitation.level > WeatherType.PrecipitationLevel.None &&
+      _.precipitation.type === WeatherType.PrecipitationType.Snow
+  );
+}
+
+function willRain(weatherTypes) {
+  return weatherTypes.some(
+    (_) => _.precipitation.level > WeatherType.PrecipitationLevel.None
+  );
+}
+
+function willBeClear(weatherTypes) {
+  return weatherTypes.every((_) => _.clouds.isClear());
+}
---
+++ server/src/domain/forecastLayer.test.js
@@ -0,0 +1,40 @@
+const forecastLayer = require("./forecastLayer");
+const weatherRepoBuilder = require("../infrastructure/hardcodedWeatherRepository");
+const forecastPromise = weatherRepoBuilder().get({});
+
+describe("forecastLayer", () => {
+  describe("#build", () => {
+    describe("when the forecast is null", () => {
+      it("returns an empty layer", () => {
+        expect(forecastLayer.build(null)).toEqual({
+          inner: { colours: [] },
+          outer: { colours: [] },
+        });
+      });
+    });
+
+    describe("when forecast includes weather data", () => {
+      const buildLayer = forecastPromise.then((_) => forecastLayer.build(_));
+
+      it("sets the startIndex based on the current hour in UTC in the weather data", async () => {
+        expect((await buildLayer).inner.startIndex).toEqual(0);
+      });
+
+      it("returns 60 lights in the inner layer", async () => {
+        expect(await (await buildLayer).inner.colours.length).toEqual(60);
+      });
+
+      it("has colours for each light based on the hour in the forecast data", async () => {
+        expect(await (await buildLayer).inner.colours).toEqual(
+          [
+            Array(5).fill("grey"),
+            Array(15).fill("blue"),
+            Array(5).fill("white"),
+            Array(5).fill("yellow"),
+            Array(30).fill("grey"),
+          ].flat()
+        );
+      });
+    });
+  });
+});
